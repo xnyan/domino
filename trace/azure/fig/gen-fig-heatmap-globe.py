e96320cfcc2c89cbff520157424cf19364283566
xnyan <email>
2020-11-07 20:04:58 -0500

    added a script for generating all roundtrip delay heatmap figures for the globe setting

---
+++ trace/azure/fig/gen-fig-heatmap-globe.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+import heat as h
+
+dir='trace-azure-globe-6dc-24h-202005170045-202005180045'
+src_dc_l = ['eastus2', 'westus2', 'francecentral', 'australiaeast', 'eastasia', 'southeastasia']
+dst_dc_l = ['eastus2', 'westus2', 'francecentral', 'australiaeast', 'eastasia', 'southeastasia']
+for src in src_dc_l:
+  for dst in dst_dc_l:
+    if dst is src:
+      continue
+    df=dir+'/' + src+'-'+dst+'.log.txt'
+    print df
+    ##h.lat_heat_map(df, 1, 1, 12, 1, 'min', 'ms', 'ms')
+    #h.vertical_lat_heat_map(df, 1, 1, 12, 1, 'min', 'ms', 'ms')
+    #h.lat_heat_map(df, 1, 1, 15, 1, 'min', 'ms', 'ms')
+    h.lat_heat_map(df, 1, 1, 11, 1, 'min', 'ms', 'ms')
+
